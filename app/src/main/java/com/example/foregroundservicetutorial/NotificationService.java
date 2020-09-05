fadf81061457978b3206aa46aaa05cbb5a6a650a
zixin <czixin@gmail.com>
2020-09-05 20:28:19 +0800

    added NotificationService class

---
+++ app/src/main/java/com/example/foregroundservicetutorial/NotificationService.java
@@ -0,0 +1,108 @@
+package com.example.foregroundservicetutorial;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class NotificationService extends Service {
+    private static final int ID_SERVICE = 101;
+
+    @Nullable
+    @Override public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override public void onCreate() {
+        super.onCreate();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String channelId = createNotificationChannel(notificationManager);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId);
+
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setContentTitle("Navigine Service Example")
+                .setCategory(NotificationCompat.CATEGORY_SERVICE)
+                .build();
+
+        startForeground(ID_SERVICE, notification);
+    }
+
+    @Override public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        // start timer to run every minute, write to file
+
+        return START_REDELIVER_INTENT;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String createNotificationChannel(NotificationManager notificationManager){
+        String channelId = "NAVIGINE_SERVICE_CHANNEL_ID_EXAMPLE";
+        String channelName = "NAVIGINE CHANNEL NAME EXAMPLE";
+        NotificationChannel channel = new NotificationChannel(channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC); // will be shown in lock screen
+        notificationManager.createNotificationChannel(channel);
+        return channelId;
+    }
+
+    // functions to read/write file
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("location.txt", Context.MODE_APPEND));
+            outputStreamWriter.write(data + "\n");
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("location.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append("\n").append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("location activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("location activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+}
